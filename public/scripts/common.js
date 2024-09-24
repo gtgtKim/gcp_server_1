546f88e086e2e05cfa1176fff9dcba74424eee39
gtgtKim <kgtrlarbxo@gmail.com>
2024-09-24 11:17:32 +0900

    fpm gtm code edit

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,3 +1,14 @@
+// (function (w, d, s, l, i) {
+//   w[l] = w[l] || [];
+//   w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+//   var f = d.getElementsByTagName(s)[0],
+//     j = d.createElement(s),
+//     dl = l != "dataLayer" ? "&l=" + l : "";
+//   j.async = true;
+//   j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+//   f.parentNode.insertBefore(j, f);
+// })(window, document, "script", "dataLayer", "GTM-M9XFPNCW");
+
 (function (w, d, s, l, i) {
   w[l] = w[l] || [];
   w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
@@ -5,6 +16,6 @@
     j = d.createElement(s),
     dl = l != "dataLayer" ? "&l=" + l : "";
   j.async = true;
-  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  j.src = "/metrics/?id=" + i + dl;
   f.parentNode.insertBefore(j, f);
-})(window, document, "script", "dataLayer", "GTM-M9XFPNCW");
+})(window, document, "script", "dataLayer", "");
